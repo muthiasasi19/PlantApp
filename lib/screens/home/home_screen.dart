8b8f6ec8ffaa000b0d9d06aed4699c8a122096d3
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-05-21 18:27:49 +0700

    Tambah dependenci dan  tampilan awal dengan AppBar dan BottomNavBar

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+//import 'package:plant_app/svg.dart';
+import 'package:plant_app/components/my_bottom_nav_bar.dart';
+import 'package:plant_app/screens/home/components/body.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Body(),
+      bottomNavigationBar: MyBottomNavBar(),
+    );
+  }
+}
