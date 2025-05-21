d0e993985086e11d805a5c690dcdb57890c2c3cd
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-05-21 18:33:19 +0700

    Tambah daftar tanaman unggulan scroll horizontal

---
+++ lib/screens/home/components/featurred_plants.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+
+class FeaturedPlants extends StatelessWidget {
+  const FeaturedPlants({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: <Widget>[
+          FeaturePlantCard(
+            image: "assets/images/bottom_img_1.png",
+            press: () {},
+          ),
+          FeaturePlantCard(
+            image: "assets/images/bottom_img_2.png",
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
