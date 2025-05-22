d57740fbb25dd770dc3947ca87a125ef430aaa78
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-05-23 04:10:04 +0700

    Buat State peta: loading & loaded dengan data posisi dan alamat.

--- lib/bloc/map_state.dart
+++ lib/bloc/map_state.dart
@@ -1,1 +1,32 @@
+part of 'map_bloc.dart';
 
+abstract class MapState {
+  const MapState();
+}
+
+class MapLoading extends MapState {
+  const MapLoading();
+}
+
+class MapLoaded extends MapState {
+  final Position currentPosition;
+  final String currentAddress;
+  final LatLng? pickedLatLng;
+  final String? pickedAddress;
+
+  const MapLoaded({
+    required this.currentPosition,
+    required this.currentAddress,
+    this.pickedLatLng,
+    this.pickedAddress,
+  });
+
+  MapLoaded copyWith({LatLng? pickedLatLng, String? pickedAddress}) {
+    return MapLoaded(
+      currentPosition: currentPosition,
+      currentAddress: currentAddress,
+      pickedLatLng: pickedLatLng,
+      pickedAddress: pickedAddress,
+    );
+  }
+}
