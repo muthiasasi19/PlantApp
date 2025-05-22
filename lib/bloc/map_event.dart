fc43a13b611e0f0e11f49abcfa761c958b724a13
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-05-23 03:59:56 +0700

    Tambah event untuk MapBloc (load, pick, clear lokasi)

--- lib/bloc/map_event.dart
+++ lib/bloc/map_event.dart
@@ -1,1 +1,27 @@
+part of 'map_bloc.dart';
 
+abstract class MapEvent {
+  const MapEvent();
+}
+
+class MapLoadStarted extends MapEvent {
+  const MapLoadStarted();
+}
+
+class MapLocationPicked extends MapEvent {
+  final LatLng latLng;
+
+  const MapLocationPicked(this.latLng);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is MapLocationPicked && latLng == other.latLng;
+
+  @override
+  int get hashCode => latLng.hashCode;
+}
+
+class MapClearPicked extends MapEvent {
+  const MapClearPicked();
+}
