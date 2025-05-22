080b69be290099e27ae6c6aa08b05fb1eeb41daf
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-05-23 03:26:59 +0700

    Buat code camera_event

--- lib/bloc/camera_event.dart
+++ lib/bloc/camera_event.dart
@@ -1,1 +1,35 @@
+part of 'camera_bloc.dart';
 
+sealed class CameraEvent {}
+
+final class InitializeCamera extends CameraEvent {}
+
+final class SwitchCamera extends CameraEvent {}
+
+final class ToggleFlash extends CameraEvent {}
+
+final class TakePicture extends CameraEvent {
+  final void Function(File imageFile) onPictureTaken;
+  TakePicture(this.onPictureTaken);
+}
+
+final class TapToFocus extends CameraEvent {
+  final Offset position;
+  final Size previewSize;
+  TapToFocus(this.position, this.previewSize);
+}
+
+final class PickImageFromGallery extends CameraEvent {}
+
+final class OpenCameraAndCapture extends CameraEvent {
+  final BuildContext context;
+  OpenCameraAndCapture(this.context);
+}
+
+final class DeleteImage extends CameraEvent {}
+
+final class ClearSnackbar extends CameraEvent {}
+
+final class RequestPermissions extends CameraEvent {}
+
+final class SaveImageAsProfile extends CameraEvent {}
