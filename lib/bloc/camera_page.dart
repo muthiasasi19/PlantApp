1ee69f60e44c6a52406c1481c6607a69622b58f1
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-05-23 03:29:37 +0700

    Buat tampilan awal CameraPage

--- lib/bloc/camera_page.dart
+++ lib/bloc/camera_page.dart
@@ -1,1 +1,47 @@
+import 'dart:io';
 
+import 'package:camera/camera.dart';
+import 'package:plant_app/bloc/camera_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({super.key});
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  @override
+  void initState() {
+    super.initState();
+    final bloc = context.read<CameraBloc>();
+    bloc.add(RequestPermissions());
+    //bloc.add(InitializeCamera());
+  }
+
+  IconData _flasIcon(FlashMode mode) {
+    return switch (mode) {
+      FlashMode.auto => Icons.flash_auto,
+      FlashMode.always => Icons.flash_on,
+      _ => Icons.flash_off,
+    };
+  }
+
+  Widget _circleButton(IconData icon, VoidCallback onTap) {
+    return ClipOval(
+      child: Material(
+        color: Colors.white24,
+        child: InkWell(
+          onTap: onTap,
+          child: SizedBox(
+            width: 50,
+            height: 50,
+            child: Icon(icon, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
